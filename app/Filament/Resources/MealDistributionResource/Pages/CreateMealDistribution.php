b598aad655b3c0ebb36d8d504186221bd4efd39e
anwar_Alhitar <anwaralhitar@gmail.com>
2025-02-22 17:16:57 +0300

    CreateMealDistribution

---
+++ app/Filament/Resources/MealDistributionResource/Pages/CreateMealDistribution.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\MealDistributionResource\Pages;
+
+use App\Filament\Resources\MealDistributionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateMealDistribution extends CreateRecord
+{
+    protected static string $resource = MealDistributionResource::class;
+}
