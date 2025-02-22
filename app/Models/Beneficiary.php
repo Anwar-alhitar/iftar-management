eb95f0ae53cabafa80b8a9db3fe000b30e408040
anwar_Alhitar <anwaralhitar@gmail.com>
2025-02-22 17:19:07 +0300

    Beneficiary

--- app/Models/Beneficiary.php
+++ app/Models/Beneficiary.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Beneficiary extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'full_name',
+        'id_number',
+      
+        'gender'
+    ];
+
+    public addSerial_namper(){
+        static::creating(function ($beneficiary) {
+            $maxSerial = Beneficiary::where('gender', $beneficiary->gender)
+                ->max('serial_number');
+
+            $beneficiary->serial_number = ($maxSerial ?? 0) + 1;
+        });
+    }
+
 }
