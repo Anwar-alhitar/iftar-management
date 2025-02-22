5a7e77870372cccdca3bb4e19109482e666c4536
anwar_Alhitar <anwaralhitar@gmail.com>
2025-02-22 17:19:25 +0300

    MealDistribution

--- app/Models/MealDistribution.php
+++ app/Models/MealDistribution.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class MealDistribution extends Model
 {
     use HasFactory;
+
+    protected static function booted()
+    {
+        static::creating(function ($beneficiary) {
+            $maxSerial = Beneficiary::where('gender', $beneficiary->gender)
+                ->max('serial_number');
+
+            $beneficiary->serial_number = ($maxSerial ?? 0) + 1;
+        });
+    }
+
+    public function mealDistributions()
+    {
+        return $this->hasMany(MealDistribution::class);
+    }
 }
