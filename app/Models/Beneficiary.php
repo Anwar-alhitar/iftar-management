21ee278b473ecb55c54ec6af147009860e8af606
anwar_Alhitar <anwaralhitar@gmail.com>
2025-02-22 17:59:08 +0300

    add the functionality of generate serial namber

--- app/Models/Beneficiary.php
+++ app/Models/Beneficiary.php
@@ -16,12 +16,21 @@ class Beneficiary extends Model
         'gender'
     ];
 
-    public addSerial_namper(){
+    protected static function booted()
+    {
         static::creating(function ($beneficiary) {
-            $maxSerial = Beneficiary::where('gender', $beneficiary->gender)
-                ->max('serial_number');
-
-            $beneficiary->serial_number = ($maxSerial ?? 0) + 1;
+            $beneficiary->serial_number = Beneficiary::where('gender', $beneficiary->gender)
+                ->max('serial_number') + 1;
+        });
+    
+        static::saving(function ($beneficiary) {
+            $exists = Beneficiary::where('gender', $beneficiary->gender)
+                ->where('serial_number', $beneficiary->serial_number)
+                ->exists();
+                
+            if ($exists) {
+                throw new \Exception('الرقم التسلسلي موجود مسبقًا لهذا النوع');
+            }
         });
     }
 
